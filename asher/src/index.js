7c17369c2d08d4b8a167e2354692242440393e2c
walcutt <walcutt.0@gmail.com>
2024-02-27 22:11:23 -0500

    Add discord bot boilerplate

--- asher/src/index.js
+++ asher/src/index.js
@@ -1,3 +1,12 @@
 import { secretKeys, getSecret } from "./secrets.js";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
-console.log(getSecret(secretKeys.BOT_KEY));
+const token = getSecret(secretKeys.BOT_KEY);
+
+const client = new Client({ intents: GatewayIntentBits.Guilds });
+
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
