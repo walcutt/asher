8573c14ed5fdb3b31fccaa6a3324fe725567d4a6
walcutt <walcutt.0@gmail.com>
2024-10-13 16:24:06 -0400

    Hush + debug

--- asher/src/routines/message-update/shame-edit.js
+++ asher/src/routines/message-update/shame-edit.js
@@ -3,9 +3,23 @@ export function shameEdit(oldMessage, newMessage) {
     const newContent = newMessage.content;
 
     const user = oldMessage.author.displayName;
+
+    const self = oldMessage.client.user;
+
     const timestamp = newMessage.createdTimestamp;
 
     const messageToSend = `${user} edited this message at <t:${timestamp}:F>. It used to say:\n${oldContent}`;
 
-    newMessage.reply(messageToSend).then((m) => m.pin());
+    console.log("Received message update event!");
+    console.log(oldMessage);
+    console.log("---------------");
+    console.log(newMessage);
+    console.log("---------------");
+    console.log(self);
+
+    if(oldMessage.user.id == self.id || newMessage.user.id === self.id) {
+        return;
+    }
+
+    // newMessage.reply(messageToSend).then((m) => m.pin());
 }
